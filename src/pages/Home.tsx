d759646f19a9288ce532e331ed7a651d9d1c19ce
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-09-28 21:11:13 -0300

    Feat: Login - Assets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonItem, IonPage, IonInput, IonButton, IonImg } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+          <IonImg
+          src="AQUI VA UN ENLACE A LA IMAGEN"
+          alt="logo"
+        ></IonImg>
+        <IonItem>
+          <IonInput label="Correo electrónico" labelPlacement="stacked" type="email" placeholder="Ingresa tu correo electrónico"></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonInput label="Contraseña" labelPlacement="stacked" type="password" placeholder="Ingresa tu contraseña"></IonInput>
+        </IonItem>
+        <IonButton size="small" fill="clear" color="tertiary">
+          Recuperar contraseña
+        </IonButton>
+        <IonButton expand="block"> 
+          Iniciar sesión
+        </IonButton>
+        <IonButton fill="outline" expand="block">
+          Registrarse
+        </IonButton>
       </IonContent>
     </IonPage>
   );
