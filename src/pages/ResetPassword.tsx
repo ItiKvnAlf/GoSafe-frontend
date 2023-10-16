6876bfa05bcb1559c69e67c4277ace6a79f1ceba
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-10-16 14:35:59 -0300

    Feat: ResetPassword Page Alerts

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,4 +1,5 @@
 import { IonContent, IonItem, IonPage, IonInput, IonButton, IonAlert } from '@ionic/react';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -7,18 +8,38 @@ const ResetPassword: React.FC = () => {
   const [showAlert, setShowAlert] = useState(false);
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
-  const [redirectToHome, setRedirectToHome] = useState(false);
-  const history = useHistory();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     document.title = 'Reestablecer contraseña';
-    if (!showSuccessAlert && redirectToHome) {
-      (history as any).push('/home');
-    }
-  }, [showSuccessAlert, redirectToHome]);
+  }, []);
 
   const handleResetPassword = async () => {
+    setLoading(true);
+
+    if (email === '') {
+      setAlertMessage('El correo electrónico es requerido');
+      setShowAlert(true);
+      setLoading(false);
+      return;
+    }
     
+    try {
+      const response = await axios.get(`http://localhost:3000/users/${email}`);
+      if (response.data !== undefined) {
+        setAlertMessage('Se ha enviado un correo electrónico con el código de verificación (TEST)');
+        setShowAlert(true);
+        setLoading(false);
+      } else {
+        setAlertMessage('Correo electrónico no registrado');
+        setShowAlert(true);
+        setLoading(false);
+      }
+    } catch (error: any) {
+      setAlertMessage('Correo electrónico no registrado');
+      setShowAlert(true);
+      setLoading(false);
+    }
   };
 
   const handleAlertConfirm = () => {
@@ -28,10 +49,10 @@ const ResetPassword: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen color="light">
-        <IonItem style ={{marginTop:"20%", marginLeft: "10%", marginRight: "10%", justifyContents:"center", alignItems:"center"}}>
+        <IonItem style ={{marginTop:"50%", marginLeft: "10%", marginRight: "10%", justifyContents:"center", alignItems:"center"}}>
           <IonInput value={email} label="Correo electrónico" labelPlacement="stacked" type="email" placeholder="Ingrese su correo electrónico" onIonInput={(e) => setEmail(e.detail.value!)}></IonInput>
         </IonItem>
-        <IonButton routerLink="home" size="small" expand="block" style ={{marginTop: "5%", marginLeft: "10%", marginRight: "10%"}}>
+        <IonButton onClick={handleResetPassword} disabled={loading} size="small" expand="block" style ={{marginTop: "5%", marginLeft: "10%", marginRight: "10%"}}>
           Enviar código de verificación
         </IonButton>
         <IonButton routerLink="login" color="medium" fill="clear" size="small" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
