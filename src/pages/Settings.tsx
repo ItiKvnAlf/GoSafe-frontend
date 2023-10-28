53f054bb8ec3f942631ba81ca42d5342b1633ed2
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-10-28 17:23:29 -0300

    Fix: Settings RouterLinks

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -7,13 +7,13 @@ const Settings: React.FC = () => {
         <IonButton fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
           Cambiar Tema
         </IonButton>
-        <IonButton routerLink="resetPassword" fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
+        <IonButton fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
           Activar autenticación dactilar
         </IonButton>
-        <IonButton routerLink="resetPassword" fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
+        <IonButton fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
           Activar autenticación facial
         </IonButton>
-        <IonButton routerLink="resetPassword" fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
+        <IonButton fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
           Configurar acceso por comando
         </IonButton>
       </IonContent>
