221ef78f56a853b21b700ad31a87fb0473af37b1
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-11-13 18:36:51 -0300

    Feat: Profile Picture Template

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,21 @@
-import { IonContent, IonItem, IonPage, IonInput, IonButton, IonIcon, IonHeader } from '@ionic/react';
+import { IonContent, IonAvatar, IonPage, IonTitle, IonButton, IonHeader } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
 
 const Profile: React.FC = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [profilePicture, setProfilePicture] = useState('');
+
+  useEffect(() => {
+    document.title = 'Perfil';
+  }, []);
+
   return (
     <IonPage>
       <IonContent fullscreen color="light">
+      <IonAvatar>
+        <img alt="profilePicture" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+      </IonAvatar>
         <IonButton fill="outline" expand="block" style ={{marginLeft: "10%", marginRight: "10%"}}>
           Modificar datos
         </IonButton>
